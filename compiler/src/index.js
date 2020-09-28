ff8db57602dc37ff1ab0d0f93c786c5bada0de83
koga <koga@wh-plus.com>
2020-09-28 17:34:04 +0900

    feat: tsconfig.jsのコード品質関連オプションを設定

--- compiler/src/index.js
+++ compiler/src/index.js
@@ -3,4 +3,18 @@ const button = document.querySelector('button');
 function clickHandler(message) {
     console.log('Clicked:' + message);
 }
+function addition(num1, num2) {
+    switch (num1) {
+        case 1:
+        case 2:
+            console.log(num1);
+            break;
+        case 3:
+            console.log(num2);
+            break;
+        default:
+            console.log(num1 + num2);
+    }
+}
+console.log(addition(1, 2));
 button.addEventListener('click', clickHandler.bind(null, 'welcome'));
--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -4,4 +4,17 @@ function clickHandler(message: string) {
     console.log('Clicked:' + message);
 }
 
+function addition(num1: number, num2: number) {
+    switch (num1){
+        case 1:
+        case 2:
+        case 3:
+            console.log(num2);
+        default:
+            console.log(num1 + num2);
+    }
+}
+
+console.log(addition(1, 2));
+
 button.addEventListener('click', clickHandler.bind(null, 'welcome'));
