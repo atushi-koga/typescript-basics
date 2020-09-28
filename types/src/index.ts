fdfacb3238f896bb83fca226b98488dd9b95e7bf
koga <koga@wh-plus.com>
2020-09-28 11:23:14 +0900

    generate error

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,14 +1,42 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
+enum Role {ADMIN = 'administrator', READ_ONLY = 'read-member', AUTHOR = 'author'};
+
+const person
+//     : {
+//     name: string;
+//     age: number;
+//     hobbies: string[],
+//     role: [number, string]
+// }
+    = {
+    name: 'Alex',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.READ_ONLY
+}
+
+type Combinable = string | number;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable, resultTypes: ConversionDescriptor): string | number {
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultTypes === 'as-number') {
+        return +input1 + +input2;
+    } else if (typeof input1 === 'string' && typeof input2 === 'string') {
+        return input1 + input2;
     } else {
-        return result;
+        return 'error';
     }
 }
 
-const num1 = 5;
-const num2 = 2.8;
-let phrase = 'Result is:';
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
 
-add(num1, num2, true, phrase);
+function printResult(r: string | number): void {
+    console.log(r);
+}
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+
+generateError('An error occurred', 500);
