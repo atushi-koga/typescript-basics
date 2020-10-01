28bba48b03f6917094c3178ac4e6fc3e2e17fdb6
koga <koga@wh-plus.com>
2020-10-01 09:51:33 +0900

    refactor: functional component でpropsを使う時、interfaceを使ってgererics型を指定する。

--- with-react/src/App.tsx
+++ with-react/src/App.tsx
@@ -4,9 +4,10 @@ import TodoList from "./components/TodoList";
 // React.FC: functional componentの型
 // React.ClassicComponent: class componentの型
 const App: React.FC = () => {
+    const todos = [{id: '1', text: 'Finish the course'}];
     return (
         <div className="App">
-            <TodoList/>
+            <TodoList items={todos}/>
         </div>
     );
 }
